7ed6191dec9ab4c21b519e61ef1f95f39d32b3a7
MASIJUN <785493116@qq.com>
2021-03-26 11:26:28 +0800

    feat some new 2021.3.26

---
+++ 02-basic-java/02-auto-shutdown/src/java/AutoShutDown.java
@@ -0,0 +1,18 @@
+package java;
+
+import java.io.IOException;
+
+public class AutoShutDown {
+    public static void main(String[] args) throws IOException {
+//        setShutDown();
+        cancelShutDown();
+    }
+
+    public static void setShutDown() throws IOException {
+        Runtime.getRuntime().exec("shutdown -s -t 3600");
+    }
+
+    public static void cancelShutDown() throws IOException {
+        Runtime.getRuntime().exec("shutdown -a");
+    }
+}
